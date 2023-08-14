aa69fa2930c6945e7201633d260755bc1b84aa63
MRensen <markrensen@hotmail.com>
2023-08-14 13:27:58 +0200

    antwoord beginnersix

---
+++ src/nl/novi/jp/methods/beginner/BeginnerSix.java
@@ -0,0 +1,11 @@
+package nl.novi.jp.methods.beginner;
+
+public class BeginnerSix {
+    public static void main(String[] args) {
+        helloWorld();
+    }
+
+    public static void helloWorld(){
+        System.out.println("Hello world");
+    }
+}
